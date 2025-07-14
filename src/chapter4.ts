97ffbb7cb390a9faa0ea5a61fa21bf0ba483859e
devdong9897 <dev.dong9897@gmail.com>
2025-07-14 14:18:50 +0900

    사용자 정의 타입가드

---
+++ src/chapter4.ts
@@ -0,0 +1,23 @@
+/**
+ * 사용자 정의 타입가드
+ */
+
+type Dog = {
+  name: string;
+  isBark: boolean;
+};
+
+type Cat = {
+  name: string;
+  isScratch: boolean;
+};
+
+type Animal = Dog | Cat;
+
+function warning(animal: Animal) {
+  if ("isBark" in animal) {
+    // 강아지
+  } else if ("isScratch" in animal) {
+    // 고양이
+  }
+}
