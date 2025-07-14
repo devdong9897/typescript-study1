558c14eed07f53891b972db9e5224a6b75742ee4
devdong9897 <dev.dong9897@gmail.com>
2025-07-14 14:47:17 +0900

    feat: 사용자 정의 타입가드 좁히기

--- src/chapter4.ts
+++ src/chapter4.ts
@@ -14,10 +14,21 @@ type Cat = {
 
 type Animal = Dog | Cat;
 
+function isDog(animal: Animal): animal is Dog {
+  // (animal as Dog): animal을 강아지처럼 간주해줘라는 뜻. 강제로 Dog 타입처럼 쓴다는 말.
+  return (animal as Dog).isBark !== undefined;
+}
+
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isScratch !== undefined;
+}
+
 function warning(animal: Animal) {
-  if ("isBark" in animal) {
+  if (isDog(animal)) {
     // 강아지
-  } else if ("isScratch" in animal) {
+    animal;
+  } else if (isCat(animal)) {
     // 고양이
+    animal;
   }
 }
